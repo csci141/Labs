289bc7e802b86743a50e5c45fa871826260da3c3
Caroline Hardin <hardinc3@wwu.edu>
2026-02-06 14:13:31 -0800

    Create turtleshape.py

---
+++ 202610/Lab 5 student materials/skel/turtleshape.py
@@ -0,0 +1,7 @@
+def save_canvas(t, base_name):
+    """ Save the turtle canvas that t lives on as an eps file called
+        {base_name}.eps. """
+    screen = t.getscreen()
+    canvas = screen.getcanvas()
+    canvas.postscript(file=base_name + '.eps')
+
